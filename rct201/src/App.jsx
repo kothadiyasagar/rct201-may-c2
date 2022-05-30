86e77637b84b51f4fdcd382354468c1060c68f7e
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-05-30 18:36:28 +0530

    done

--- rct201/src/App.jsx
+++ rct201/src/App.jsx
@@ -3,15 +3,21 @@ import { Image } from "./Components/Image";
 import { Pagination } from "./Components/Pagination";
 import Eye from './Components/eye.svg'
 import React , {useState}from "react";
+
 function App() {
   const [count , setcount] = useState(1)
+  const [type, settype]=useState(false)
+  const [email, setEmail] = useState('');
 
    const hadal= (e)=>{
        setcount(e)
    }
+   const haddal = (e)=>{
+         settype(true)
+   }
   return (
     <div className="container">
-      <Input  rightLogo={Eye}  variant='outline' />
+      <Input  rightLogo={Eye}  type={type? "password" : "text"} variant='outline'  rightLogoOnClick={(e)=>{haddal(e)}}   onChange={e => setEmail(e.target.value)}/>
 
       <Image  src="https://thumbs.dreamstime.com/b/handsome-man-black-suit-white-shirt-posing-studio-attractive-guy-fashion-hairstyle-confident-man-short-beard-125019349.jpg" 
       borderRadius="50%"
--- rct201/src/Components/Input.jsx
+++ rct201/src/Components/Input.jsx
@@ -12,8 +12,8 @@ export const Input = ({
   return (
     <div className="inputContainer">
       {/* Add Right Logo */}
-      <input type={type} className={`input`}   onChange={onChange}/>
-      <img style={{marginLeft:"-30px"}} src={rightLogo} variant={variant}   size={size}  onClick={rightLogoOnClick}   />
+      <input data-testid="email-input" type={type} className={`input`}   onChange={onChange}/>
+      <img style={{marginLeft:"-30px"}} src={rightLogo} variant={variant}   size={size}  onClick={()=>{rightLogoOnClick(true)}}   />
     </div>
   );
 };
