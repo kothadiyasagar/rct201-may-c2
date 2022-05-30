34a8e4380df33d34c1c8596b34f281f695d11e4e
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-05-30 20:56:39 +0530

    done

--- rct201/src/__tests__/App.test.js
+++ rct201/src/__tests__/App.test.js
@@ -1,5 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer"
+import App from "../App"
+import { Input } from "../Components/Input";
+import userEvent from "@testing-library/user-event"
 
-test("Fake assertion", () => {
-  expect(1).toBe(1);
-});
+describe(" Testing", function () {
+  
+  test('pass invalid email to text input value', () => {
+    render(<App/>);
+ 
+    const inputEl = screen.getByTestId("email-input");
+    userEvent.type(inputEl, "text");
+ 
+    expect(screen.getByTestId("email-input")).toHaveValue("text");
+    // expect(screen.queryByTestId("error-msg")).toBeInTheDocument();
+    // expect(screen.queryByTestId("error-msg").textContent).toEqual("Please enter a valid email.");
+  });
+
+  
+  test('pass invalid email to text input value', () => {
+    render(<App/>);
+ 
+    const inputEl = screen.getByTestId("email-input");
+    userEvent.onchange = (sagar)=>{
+          sagar=screen.getByDisplayValue(sagar)
+    }
+ 
+
+    // expect(screen.queryByTestId("error-msg")).toBeInTheDocument();
+    // expect(screen.queryByTestId("error-msg").textContent).toEqual("Please enter a valid email.");
+  });
+
+  })
